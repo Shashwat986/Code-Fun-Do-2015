bf5de9c05595bdd0191175a1efddbb4e849b00d1
Shashwat <chandra.shashwat@gmail.com>
2015-01-31 23:17:02 +0530

    Two-Column Layout

--- BlankSharedApp1/BlankSharedApp1.Shared/js/app.js
+++ BlankSharedApp1/BlankSharedApp1.Shared/js/app.js
@@ -1,4 +1,17 @@
 ﻿function onSubmit(eventInfo) {
     var userName = document.getElementById("q").value;
+    document.getElementById("output").innerHTML = userName + " is your search term.";
     
+    $(document).ready(function () {
+        $(".wait").show();
+        createStoryJS({
+            type: 'timeline',
+            width: '800',
+            height: '600',
+            source: 'http://timeline.knightlab.com/static/welcome/welcome.json',
+            embed_id: 'my-timeline'
+        });
+        $(".wait").hide();
+    });
+
 }
