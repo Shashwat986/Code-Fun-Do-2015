119d55114f8734a1156bc9243edb4a94a9f67cca
Shashwat <chandra.shashwat@gmail.com>
2015-02-01 07:13:03 +0530

    json works

--- BlankSharedApp1/BlankSharedApp1.Windows/bin/Debug/AppX/js/app.js
+++ BlankSharedApp1/BlankSharedApp1.Windows/bin/Debug/AppX/js/app.js
@@ -1,19 +1,21 @@
 ﻿function onSubmit(eventInfo) {
-    var userName = document.getElementById("q").value;
-    document.getElementById("output").innerHTML = userName + " is your search term.";
     
     $(document).ready(function () {
+        var userName = document.getElementById("q").value.trim();
+        document.getElementById("output").innerText = userName + " is your search term.";
         $("#wait-timeline").show();
         $("#wait-right").show();
-
-        createStoryJS({
+        console.log('http://172.27.20.103:1337/people/get?name=' + encodeURIComponent(userName));
+        /*createStoryJS({
             type: 'timeline',
             width: '800',
             height: '600',
-            source: 'http://172.27.20.103:1337/people/get?name=sachin%20tendulkar',
+            source: 'http://172.27.20.103:1337/people/get?name=' + encodeURIComponent(userName),
             embed_id: 'my-timeline'
-        });
+        });*/
         //source: 'http://timeline.knightlab.com/static/welcome/welcome.json',
+        //http://172.27.20.103:1337/people/get?name=sachin%20tendulkar
+        ///jsons/test.json
         $("#wait-timeline").hide();
     });
 }
