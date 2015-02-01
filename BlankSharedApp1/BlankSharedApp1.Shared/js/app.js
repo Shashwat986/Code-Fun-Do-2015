ed38ea09ec9969ccf571626b1b25361fd8c0c860
Shashwat <chandra.shashwat@gmail.com>
2015-02-01 09:27:07 +0530

    MVP

--- BlankSharedApp1/BlankSharedApp1.Shared/js/app.js
+++ BlankSharedApp1/BlankSharedApp1.Shared/js/app.js
@@ -2,10 +2,13 @@
     
     $(document).ready(function () {
         var userName = document.getElementById("q").value.trim();
+        if (userName.length == 0)
+            return;
         document.getElementById("output").innerText = userName + " is your search term.";
         $("#wait-timeline").show();
         $("#wait-right").show();
-        //console.log('http://172.27.20.103:1337/people/get?name=' + encodeURIComponent(userName));
+        console.log('http://172.27.20.103:1337/people/get?name=' + encodeURIComponent(userName));
+        $("#my-timeline").html("");
         createStoryJS({
             type: 'timeline',
             width: '800',
@@ -17,20 +20,35 @@
         //http://172.27.20.103:1337/people/get?name=sachin%20tendulkar
         ///jsons/test.json
         $("#wait-timeline").hide();
+
+        url_v = "https://api.datamarket.azure.com/Bing/Search/v1/Image?Query=%27" + encodeURIComponent(userName) + "%27&$format=json";
+        key_v = "gOW3yOZfJuQy7HEPJF05zC/DjMt8ngCEhskpk8abdbM";
+        $.ajax({
+            url: url_v,
+            dataType: 'json',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("Authorization", "Basic " + btoa(key_v + ":" + key_v));
+            }
+        }).done(function (data) {
+            console.log(data);
+            $("#wait-right").hide();
+            var images = data.d.results;
+            ims = $(".pics");
+            for (i=0;i<ims.length;i++)
+            {
+                $(ims.get(i)).attr("src", images[i].MediaUrl).show();
+            }
+            var $container = $("#right-div").isotope({
+                itemSelector: '.pics',
+                layoutMode: 'masonry'
+            });
+        });
     });
 }
 
-function init()
-{
+function init() {
     $(document).ready(function () {
-        $("#q").quickselect({data : ['Option 1', 'Option 2', 'Option 3']});
+        $("#q").quickselect({ 'ajax': '/jsons/entries.json' });
+        // 'exactMatch': 'true', 
     });
-    
-
-    /*$(document).ready(function () {
-        $.getJSON('', function (data) {
-            var list = [];
-
-        })
-    });*/
-}
+};
--- BlankSharedApp1/BlankSharedApp1.Windows/bin/Debug/AppX/js/app.js
+++ BlankSharedApp1/BlankSharedApp1.Windows/bin/Debug/AppX/js/app.js
@@ -2,35 +2,53 @@
     
     $(document).ready(function () {
         var userName = document.getElementById("q").value.trim();
+        if (userName.length == 0)
+            return;
         document.getElementById("output").innerText = userName + " is your search term.";
         $("#wait-timeline").show();
         $("#wait-right").show();
         console.log('http://172.27.20.103:1337/people/get?name=' + encodeURIComponent(userName));
-        /*createStoryJS({
+        $("#my-timeline").html("");
+        createStoryJS({
             type: 'timeline',
             width: '800',
             height: '600',
             source: 'http://172.27.20.103:1337/people/get?name=' + encodeURIComponent(userName),
             embed_id: 'my-timeline'
-        });*/
+        });
         //source: 'http://timeline.knightlab.com/static/welcome/welcome.json',
         //http://172.27.20.103:1337/people/get?name=sachin%20tendulkar
         ///jsons/test.json
         $("#wait-timeline").hide();
+
+        url_v = "https://api.datamarket.azure.com/Bing/Search/v1/Image?Query=%27" + encodeURIComponent(userName) + "%27&$format=json";
+        key_v = "gOW3yOZfJuQy7HEPJF05zC/DjMt8ngCEhskpk8abdbM";
+        $.ajax({
+            url: url_v,
+            dataType: 'json',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("Authorization", "Basic " + btoa(key_v + ":" + key_v));
+            }
+        }).done(function (data) {
+            console.log(data);
+            $("#wait-right").hide();
+            var images = data.d.results;
+            ims = $(".pics");
+            for (i=0;i<ims.length;i++)
+            {
+                $(ims.get(i)).attr("src", images[i].MediaUrl).show();
+            }
+            var $container = $("#right-div").isotope({
+                itemSelector: '.pics',
+                layoutMode: 'masonry'
+            });
+        });
     });
 }
 
-function init()
-{
+function init() {
     $(document).ready(function () {
-        $("#q").quickselect({data : ['Option 1', 'Option 2', 'Option 3']});
+        $("#q").quickselect({ 'ajax': '/jsons/entries.json' });
+        // 'exactMatch': 'true', 
     });
-    
-
-    /*$(document).ready(function () {
-        $.getJSON('', function (data) {
-            var list = [];
-
-        })
-    });*/
-}
+};
